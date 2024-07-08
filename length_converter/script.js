d000b68e6f01c58a86758b20b6474df2aac167a3
A ALLWIN <allwin4002@gmail.com>
2024-07-08 11:21:58 +0530

    Update script.js

--- length_converter/script.js
+++ length_converter/script.js
@@ -1,9 +1,9 @@
 
 function converttoinches(){
     // select variables
-    let inputcnvt=Number(document.getElementById("input-cnvt").value)
-    let result=document.getElementById("result");
-    let cmformula=2.54;
-    let convert=inputcnvt/cmformula;
+    const inputcnvt=Number(document.getElementById("input-cnvt").value)
+    const result=document.getElementById("result");
+    const cmformula=2.54;
+    const convert=inputcnvt/cmformula;
     result.innerHTML=convert.toFixed(2)+"inches";
 }
